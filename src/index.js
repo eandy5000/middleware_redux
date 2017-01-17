6d77b7fcad7f8c1f576ddbd3f6966335a3ec6bcc
Andy Eccleston <eandy5000@yahoo.com>
2017-01-17 13:56:55 -0600

    basic providers, store and reducers working

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
 
+import App from './containers/app'
+import reducers from './reducers'
+
+const createStoreWithMiddleWare = applyMiddleware()(createStore)
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleWare(reducers)} >
+    <App />
+  </Provider>,
+  document.querySelector('.container')
+)
